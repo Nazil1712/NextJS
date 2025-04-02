c6f7f4c4ff978f2acab366174996376fdf6f68e6
Nazil1712 <ndhalwala@gmail.com>
2025-04-02 15:54:13 +0530

    NextJs setup + learned basic routing

---
+++ learning-next/src/app/blog/first/page.js
@@ -0,0 +1,7 @@
+export default function First() {
+    return (
+        <>
+        <h1>This is blog/first</h1>
+        </>
+    )
+}
---
+++ learning-next/src/app/blog/second/page.js
@@ -0,0 +1,7 @@
+export default function Second() {
+  return (
+    <>
+      <h1>This is blog/second</h1>
+    </>
+  );
+}
