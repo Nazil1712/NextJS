0b7e7749b881fb3a060f5338720c3f0187b4c4ce
Nazil1712 <ndhalwala@gmail.com>
2025-04-12 15:36:40 +0530

    <Link/>

--- learning-next/src/app/(others)/page.js
+++ learning-next/src/app/(others)/page.js
@@ -1,13 +1,27 @@
-import Link from "next/link"
-
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
       <h1>Welcome to Home</h1>
-      <Link href={"/about"}>About Us</Link>
-      <br/>
-      <Link href="/profile">Profile Page</Link>
+      <Link href={"/about"}>
+        <p className="border m-5 rounded-2xl border-blue-600 inline-block hover:bg-blue-600 hover:text-white p-3 hover:cursor-pointer">
+          About Us
+        </p>
+      </Link>
+      <br />
+      <Link href="/profile">
+        <p className="border m-5 rounded-2xl border-blue-600 inline-block hover:bg-blue-600 hover:text-white p-3 hover:cursor-pointer">
+          Profile Page
+        </p>
+      </Link>
+
+      <br />
+      <Link href={"/products"}>
+        <p className="border m-5 rounded-2xl border-blue-600 inline-block hover:bg-blue-600 hover:text-white p-3 hover:cursor-pointer">
+          View Products
+        </p>
+      </Link>
     </>
   );
 }
--- learning-next/src/app/(others)/products/page.js
+++ learning-next/src/app/(others)/products/page.js
@@ -1,11 +1,29 @@
+import Link from "next/link";
+
 export default function ProductList() {
-    return (
-        <>
-        <h1>Product List</h1>
-        <h2>Product 1</h2>
-        <h2>Product 2</h2>
-        <h2>Product 3</h2>
-        <h2>Product 4</h2>
-        </>
-    )
+  return (
+    <>
+      <h1>Product List</h1>
+      <Link href={`/products/1`}>
+        <p className="border rounded-2xl border-blue-600 inline-block hover:bg-blue-600 hover:text-white p-3 hover:cursor-pointer m-5">
+          Product 1
+        </p>
+      </Link>
+      <Link href={`/products/2`}>
+        <p className="border rounded-2xl border-blue-600 inline-block hover:bg-blue-600 hover:text-white p-3 hover:cursor-pointer m-5">
+          Product 2
+        </p>
+      </Link>
+      <Link href={`/products/3`} replace>
+        <p className="border rounded-2xl border-blue-600 inline-block hover:bg-blue-600 hover:text-white p-3 hover:cursor-pointer m-5">
+          Product 3
+        </p>
+      </Link>
+      <Link href={`/products/4`}>
+        <p className="border rounded-2xl border-blue-600 inline-block hover:bg-blue-600 hover:text-white p-3 hover:cursor-pointer m-5">
+          Product 4
+        </p>
+      </Link>
+    </>
+  );
 }
