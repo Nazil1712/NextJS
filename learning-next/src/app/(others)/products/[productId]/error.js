25349ed7d1972f4d48fd5dcecab590829dd0b575
Nazil1712 <ndhalwala@gmail.com>
2025-04-27 16:45:14 +0530

    Recovering from Errors

--- learning-next/src/app/(others)/products/[productId]/error.js
+++ learning-next/src/app/(others)/products/[productId]/error.js
@@ -1,12 +1,30 @@
-"use client"
+"use client";
+import { startTransition } from "react";
+import {useRouter} from "next/navigation";
 
+export default function Error({ error, reset }) {
+  //   console.log(error.message);
 
-export default function ErrorBoundary({error}) {
+  const router = useRouter();
 
-    console.log(error.message)
-    return(
-        <>
-        <h1 className="text-5xl text-red-600 m-5">!!!! {error.message} !!!!</h1>
-        </>
-    )
+  const reload = () =>{
+    startTransition(()=>{
+        router.refresh()
+        reset()
+    })
+  }
+
+  return (
+    <>
+      <div className="m-5">
+        <h1 className="text-5xl text-red-600">!!!! {error.message} !!!!</h1>
+        <button
+          className="bg-amber-500 p-2 rounded-2xl text-white mt-5 hover:bg-amber-400 cursor-pointer"
+          onClick={reload}
+        >
+          Try Again
+        </button>
+      </div>
+    </>
+  );
 }
