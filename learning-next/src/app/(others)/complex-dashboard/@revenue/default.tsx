a6dc6e5801441f2ea627a76fa3ec2e36f824ec56
Nazil1712 <ndhalwala@gmail.com>
2026-02-11 12:44:56 +0530

    Unmatched routes

---
+++ learning-next/src/app/(others)/complex-dashboard/@revenue/default.tsx
@@ -0,0 +1,12 @@
+import { Card } from '@/components/Card'
+import React from 'react'
+
+const RevenueMetrics = () => {
+  return (
+    <Card>
+      (From default.tsx) Revenue Metrics
+    </Card>
+  )
+}
+
+export default RevenueMetrics
---
+++ learning-next/src/app/(others)/complex-dashboard/@users/default.tsx
@@ -0,0 +1,12 @@
+import { Card } from '@/components/Card'
+import React from 'react'
+
+const UserAnalytics = () => {
+  return (
+    <Card >
+      (From default.tsx) User Analytics
+    </Card>
+  )
+}
+
+export default UserAnalytics
