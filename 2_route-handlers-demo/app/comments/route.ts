9a2d6960cad29abe0d718f51510342b7b0c8e3cc
Nazil1712 <ndhalwala@gmail.com>
2026-02-13 18:42:53 +0530

    URL Query params:

--- 2_route-handlers-demo/app/comments/route.ts
+++ 2_route-handlers-demo/app/comments/route.ts
@@ -1,20 +1,27 @@
+import { NextRequest } from "next/server";
 import { comments } from "./data";
 
-export function GET() {
-  return Response.json(comments)
+export function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const query = searchParams.get("query");
+  const filteredCommments = query
+    ? comments.filter((comment) => comment.text.includes(query))
+    : comments;
+
+    console.log(query)
+  return Response.json(filteredCommments);
 }
 
-
 export async function POST(request: Request) {
-    const comment = await request.json();
-    const newComment = {
-        id: comments.length + 1,
-        text: comment.text
-    }
+  const comment = await request.json();
+  const newComment = {
+    id: comments.length + 1,
+    text: comment.text,
+  };
 
-    comments.push(newComment)
-    return new Response(JSON.stringify(newComment),{
-        headers: {"Content-Type" : "application/json"},
-        status: 201
-    })
+  comments.push(newComment);
+  return new Response(JSON.stringify(newComment), {
+    headers: { "Content-Type": "application/json" },
+    status: 201,
+  });
 }
