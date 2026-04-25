0ba0de405b34f9e5a675b98cc9b3750ed75b5bc6
Nazil1712 <ndhalwala@gmail.com>
2026-04-25 16:57:48 +0530

    Cookies in Route Handlers

--- 2_route-handlers-demo/app/profile/api/route.ts
+++ 2_route-handlers-demo/app/profile/api/route.ts
@@ -1,25 +1,58 @@
-import { headers } from "next/headers";
+import { cookies, headers } from "next/headers";
 import { type NextRequest } from "next/server";
 
+// export async function GET(request: NextRequest) {
+//     /* There are 2 ways of reading headers */
+
+//     // 1)
+//     /* const requestHeaders = new Headers(request.headers)
+//     console.log("Request  Headeers", requestHeaders) */
+
+//     // 2)
+//     const headersList = await headers(); // This return only read only me==
+//     console.log(headersList.get("Authorization"))
+
+
+//     // return new Response("Profile API Data")
+
+//     /* How to set Custom headers */
+//     return new Response("<h1>Profile API Data",{
+//         headers: {
+//             "Content-Type":"text/html"
+//         }
+//     })
+
+// }   
+
+
+
+/* Ways to hadle Cookies */
 export async function GET(request: NextRequest) {
-    /* There are 2 ways of reading headers */
 
-    // 1)
-    /* const requestHeaders = new Headers(request.headers)
-    console.log("Request  Headeers", requestHeaders) */
+    // 1st Approach
+    /* const theme = request.cookies.get("theme")
+    console.log(theme)
 
-    // 2)
-    const headersList = await headers(); // This return only read only me==
-    console.log(headersList.get("Authorization"))
-
-
-    // return new Response("Profile API Data")
-
-    /* How to set Custom headers */
-    return new Response("<h1>Profile API Data",{
+    return new Response("<h1> Profile API Data </h1>", {
         headers: {
-            "Content-Type":"text/html"
+            'Set-Cookie': "theme=dark"
         }
-    })
+    }) */
 
-}   
+
+    // 2nd Approach
+    const cookieStore = await cookies();
+    cookieStore.set("resultsPerPage","20");
+
+    // To get
+    const results = cookieStore.get("resultsPerPage")
+    
+    console.log("Cookies",results)
+    
+    // Other Methods ==> cookieStore.has(), cookieStore.delete()
+
+    return new Response("OK")
+
+
+
+}
